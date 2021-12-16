d25584fe06f730bc4ff3b5ea4689781f9915318c
sujan210 <sujankunwar2021@gmail.com>
2021-12-16 16:34:36 +0545

    header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,16 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="{{ asset('/css/app.css')}}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="/css/header.css">
 </head>
-<body>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-5">
-            <h4>Hello</h4>
-        </div>
-        <div class="col-md-5">
-            Hi
-        </div>
+<body >
+    
+    <div class="container mb-5" >
+        <div class="row">
+            <div class="col-md-2">
+          
+      <img src="{{ asset('/image/logo.png')}}" alt="logo" style="width:6em; height:6em;">
+</div>
+
+   
+        
+
+
+            <div class="col-md-3">
+           
+                  <p>Government of Nepal</p> 
+      <p>Ministry of Federal Affairs and General<br>Administration<br>  Rastriya Kitabkhana (Nijamati)</p>
+
+</div>
+
+<div class="col-md-5" >
+       <p>"निजामती सेवा दिवस २०७८-८२ को नारा : "व्यावसायिक र सिर्जनशील प्रशासन : विकास, समृद्धि र सुशासन"</p>
+
+</div>
+
+<div class="col-md-2">
+<img src="{{ asset('/image/nepal-flag.gif')}}" alt="logo" style="width:7em; height:6em;">
+
+</div>
+
+       
+     
+     
+    
+      
+      </div>
+        
       </div>
 
 
