6d9d7666b6c5ef268422c7b98e2269801f428d7d
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2021-12-17 15:06:18 +0545

    Small changes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,24 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="{{ asset('/css/app.css')}}" rel="stylesheet">
+    <link href="{{ asset('/css/app.js')}}" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/css/header.css">
+
+    <style>
+        @yield('style')
+    </style>
 </head>
 <body >
-    
+
     <div class="container mb-5" >
         <div class="row">
             <div class="col-md-2">
-          
+
       <img src="{{ asset('/image/logo.png')}}" alt="logo" style="width:6em; height:6em;">
 </div>
 
-   
-        
+
+
 
 
             <div class="col-md-3">
-           
-                  <p>Government of Nepal</p> 
+
+                  <p>Government of Nepal</p>
       <p>Ministry of Federal Affairs and General<br>Administration<br>  Rastriya Kitabkhana (Nijamati)</p>
 
 </div>
@@ -38,13 +43,13 @@
 
 </div>
 
-       
-     
-     
-    
-      
+
+
+
+
+
       </div>
-        
+
       </div>
 
 
@@ -53,7 +58,7 @@
 
     <div class="main">
         @yield('content')
-      
+
     </div>
     <div class="footer">
     @include('layouts.footer')
