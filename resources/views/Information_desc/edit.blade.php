ced02dfb497599331be3ad4d0ee334ae1f75435f
chapagainsuman427 <chapagainsuman427@gmail.com>
2021-12-31 13:38:50 +0545

    Information

--- resources/views/Information_desc/edit.blade.php
+++ resources/views/Information_desc/edit.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('/css/app.css')}}" rel="stylesheet">
-    <script src="{{asset('js/app.js')}}"></script>
-    <link rel="stylesheet" href="/css/informationdesc.css">
+@extends('admin.dashboard')
 
-  
+@section('content')
+    
 
-    <title>informationdescs</title>
-</head>
-<body>
 
     <h3 style="text-align: center; font-weight: bold; margin-top: 5px;">Add New Information</h3>
 
@@ -54,10 +44,14 @@
         </div>
     </div>
 
-</body>
+
 
 <script src="{{ asset('js/nepalify.js') }}"></script>
 <script src="{{ asset('js/app.js') }}"></script>
+<link href="{{ asset('/css/app.css')}}" rel="stylesheet">
+<script src="{{asset('js/app.js')}}"></script>
+<link rel="stylesheet" href="/css/informationdesc.css">
+
 <script>
    
 
@@ -75,4 +69,4 @@
     });
 </script>
 
-</html>
+@endsection
